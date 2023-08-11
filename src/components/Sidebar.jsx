46704f39a8c7999060e11b843a43631d26ca1ff2
juanjosebl2 <juanjosebl2@gmail.com>
2023-08-11 15:54:40 +0200

    fixed deploy v1

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ export const Sidebar = () => {
     const { logout, user } = useAuth({ middleware: 'auth' })
 
     // Change array for put in first place category All
-    const allCategoryIndex = categories.findIndex(category => category.name === 'All');
+    const allCategoryIndex = categories && categories.findIndex(category => category.name === 'All');
     if (allCategoryIndex !== -1) {
         const allCategory = categories.splice(allCategoryIndex, 1)[0];
         categories.unshift(allCategory);
